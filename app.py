66abc2cfeec2b6c32d8dff18e35697e2f6ba8d5f
Yasmine <ykordjazy@frey.de>
2025-11-14 23:50:36 +0100

    refined the code a bit by adding user input and a loop

--- app.py
+++ app.py
@@ -2,4 +2,14 @@ import random
 
 metalrecipes = ["Brutal Chickpea Curry", "Crushing Black Bean Tacos", "Dark & Grim Lentil Tacos"]
 
-print(random.sample(metalrecipes, 1))
+print("Today's metal vegan recipe is", random.choice(metalrecipes), "🤘🔥")
+
+while True:
+  user_input = input("Do you want another random recipe? Enter y or n: ")
+  if user_input.lower() == "y":
+    print("Your next metal vegan recipe is", random.choice(metalrecipes), "🤘🔥")
+  elif user_input.lower() == "n":
+    print("Okay, goodbye!")
+    break
+  else:
+    print("Error! Try again...")
